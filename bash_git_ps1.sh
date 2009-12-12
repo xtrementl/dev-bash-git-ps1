a52ff0f112ae3b08603b47ac68e7a4b3b7c072cc
xtrementl <xtrementl@gmail.com>
2009-12-11 17:51:57 -0700

    Restructured script to be more concise and finally work properly with accurate color codes and escaping.

--- bash_git_ps1.sh
+++ bash_git_ps1.sh
@@ -46,58 +46,60 @@ function __git_branch_name {
     fi
 }
 
-# prints number of commits that are on the master but not on non-master branch
+# gets number of commits that are on the master but not on non-master branch
 function __git_trunk_unmerged_count {
     __git_branch_name
     if [ "$?" -ne 0 -o "$BRANCH" = "master" ]; then return; fi
 
     # print formatted commit count
     __git_branch_name 'parent'
-    local count=`git log HEAD..$BRANCH --format=oneline 2> /dev/null | wc -l` || return
-    if [ "$count" != "0" ]; then
-        printf "%s-%s%s" $RED $count $RESET
-    fi
+    GIT_UM_COUNT=`git log HEAD..$BRANCH --format=oneline 2> /dev/null | wc -l` || return
 }
 
-# prints number of commits that are on the branch but not on master (origin if on master branch)
+# gets number of commits that are on the branch but not on master (origin if on master branch)
 function __git_new_on_branch_count {
     __git_branch_name 'parent'
     if [ "$?" -ne 0 ]; then return; fi
 
-    # print formatted commit count
-    local count=`git log $BRANCH..HEAD --format=oneline 2> /dev/null | wc -l` || return
+    # get commit count
+    GIT_CM_COUNT=`git log $BRANCH..HEAD --format=oneline 2> /dev/null | wc -l` || return
+}
 
-    if [ "$count" != "0" ]; then
-        printf "%s+%s%s" $GREEN $count $RESET
+# build combined (+/-) counts for related commits
+function __git_counts {
+    local red="\[\033[01;31m\]"
+    local green="\[\033[01;32m\]"
+    local reset="\[\033[0m\]"
+
+    GIT_COUNT_STR=""
+    # get our counts
+    __git_new_on_branch_count
+    __git_trunk_unmerged_count
+
+    if [ "$GIT_CM_COUNT" -ne 0 ]; then
+        GIT_COUNT_STR="$green+$GIT_CM_COUNT"
+    fi
+
+    if [ "$GIT_UM_COUNT" -ne 0 ]; then
+        if [ -n "$GIT_COUNT_STR" ]; then
+            GIT_COUNT_STR="$GIT_COUNT_STR$reset/"
+        fi
+        GIT_COUNT_STR="$GIT_COUNT_STR$red-$GIT_UM_COUNT"
+    fi
+
+    if [ -n "$GIT_COUNT_STR" ]; then
+        GIT_COUNT_STR=" ($GIT_COUNT_STR$reset)"
     fi
 }
 
-# prints combined (+/-) counts for related commits
-function __git_print_counts {
-    local arr=("$(__git_new_on_branch_count)" "$(__git_trunk_unmerged_count)")
-    local str=""
+# install git integration into PS1
+function __gitify_ps1 {
+    local grey="\[\033[0;37m\]"
+    local cyan="\[\033[01;36m\]"
+    local yellow="\[\033[01;33m\]"
+    local reset="\[\033[0m\]"
 
-    # build counts
-    for item in ${arr[*]}; do
-        str="$str$item/" 
-    done
-    local len=${#str}
-    ((len--))
-    
-    # print formatted count list
-    if [ -n "$str" ]; then
-        echo -en " (${str:0:$len})"
-    fi
+    __git_counts
+    PS1="[$grey\h: $cyan\W$reset]$yellow\$(__git_ps1 ' %s')$reset$GIT_COUNT_STR \$ "
 }
-
-# setup colors
-RESET="\e[00m"
-RED="\e[01;31m"
-GRAY="\e[0;37m"
-CYAN="\e[01;36m"
-BLUE="\e[01;34m"
-GREEN="\e[01;32m"
-YELLOW="\e[01;33m"
-
-# integrate git awareness into bash terminal PS1
-PS1="\[[$GRAY\h:$RESET $CYAN\W$RESET]$YELLOW\]\$(__git_ps1 ' %s')\[$RESET\]\$(__git_print_counts) \$ "
+PROMPT_COMMAND=__gitify_ps1
