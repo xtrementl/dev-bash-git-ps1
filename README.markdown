8a485929170a89e8bb8e457abcde85fc7646240f
xtrementl <xtrementl@brokenresolve.com>
2011-04-03 23:23:51 -0600

    Updated docs format

---
+++ README.markdown
@@ -0,0 +1,33 @@
+Developer's Bash-GIT PS1 Integration
+----------
+
+### Description
+Provides simple information about git repository for the bash terminal
+
+Erik Johnson (xtrementl)
+Created: 07-29-2009
+Updated: 03-31-2011
+
+Special thanks to:
+    reborg
+
+The PS1 will be formatted as follows:
+    Non-Git repo:
+    ``[{host}: {dir}] -->``
+    ``$``
+
+    Git repo:
+    ``[{host}: {dir}] {branch}({diff upstream counts}){working dir syms} [{time last commit}] -->``
+    ``$``
+
+### Notes
+    The marker ($) will be colored red/green depending on the result of last command's exit code
+    For Git repos, the working dir symbols are:
+        + - staged changes
+        * - unstaged changes
+        ^ - stashed changes
+        % - untracked files
+
+### Installation
+Add the following line to your .bashrc:
+    ``source ~/.bash_git_ps1.sh``
--- bash_git_ps1.sh
+++ bash_git_ps1.sh
@@ -3,34 +3,35 @@
 #-----------------------------------------------------------------------------------------------
 # Developer's Bash-GIT PS1 Integration
 #-----------------------------------------------------------------------------------------------
-# Provides simple information about git repository for the bash terminal
 #
-# The PS1 will be formatted as follows:
+# Description:
+#   Provides simple information about git repository for the bash terminal
 #
-#  Non-Git repo:
-#    [{host}: {dir}] -->
-#    $
-#
-#  Git repo:
-#    [{host}: {dir}] {branch}({diff upstream counts}){working dir syms} [{time last commit}] -->
-#    $
-#
-# NOTE:
-#  The marker ($) will be colored red/green depending on the result of last command's exit code
-#  For Git repos, the working dir symbols are:
-#   + - staged changes
-#   * - unstaged changes
-#   ^ - stashed changes
-#   % - untracked files
-#
-# Erik Johnson (xtrementl)
+#   Erik Johnson (xtrementl)
 #   Created: 07-29-2009
 #   Updated: 03-31-2011
 #
-# Special thanks to:
-#   reborg
+#   Special thanks to:
+#     reborg
 #
-# Installation
+#   The PS1 will be formatted as follows:
+#       Non-Git repo:
+#       [{host}: {dir}] -->
+#       $
+#
+#       Git repo:
+#       [{host}: {dir}] {branch}({diff upstream counts}){working dir syms} [{time last commit}] -->
+#       $
+#
+# Notes:
+#   The marker ($) will be colored red/green depending on the result of last command's exit code
+#   For Git repos, the working dir symbols are:
+#       + - staged changes
+#       * - unstaged changes
+#       ^ - stashed changes
+#       % - untracked files
+#
+# Installation:
 #   Add the following line to your .bashrc:
 #       source ~/.bash_git_ps1.sh
 #-----------------------------------------------------------------------------------------------
